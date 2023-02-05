24f7e6984ff76a607951dec29a55b9ab47b87c36
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-05 17:06:08 -0500

    finished comment model

--- only_pets/models.py
+++ only_pets/models.py
@@ -24,11 +24,24 @@ class Account(db.Model):
     password = db.Column(db.String(80), nullable=False)
     posts = db.relationship('Post', back_populates='account')
 
-
 class Post(db.Model):
     """Post model."""
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    photo_url = db.Column(URLType)
+    caption = db.Column(db.String(250), nullable=False)
+    def __str__(self):
+        return f'{self.caption}'
+
+    def __repr__(self):
+        return f'{self.caption}'
+
 
 class Comment(db.Model):
     """Comment model."""
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(250), nullable=False)
+    def __str__(self):
+        return f'{self.text}'
+
+    def __repr__(self):
+        return f'{self.text}'
