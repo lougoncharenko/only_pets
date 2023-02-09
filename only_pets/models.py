af6d4a8ac9df3a7e9065494312f95a189b60cabd
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-09 09:22:00 -0500

    saving progress

--- only_pets/models.py
+++ only_pets/models.py
@@ -23,12 +23,16 @@ class Account(db.Model):
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
     posts = db.relationship('Post', back_populates='account')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
 
 class Post(db.Model):
     """Post model."""
     id = db.Column(db.Integer, primary_key=True)
     photo_url = db.Column(URLType)
     caption = db.Column(db.String(250), nullable=False)
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
     def __str__(self):
         return f'{self.caption}'
 
@@ -40,6 +44,8 @@ class Comment(db.Model):
     """Comment model."""
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(250), nullable=False)
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
     def __str__(self):
         return f'{self.text}'
 
