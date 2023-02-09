446a70a51ea1b8c096a694493ebb69ab000f337e
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-09 14:12:36 -0500

    finished route for create_account

--- only_pets/main/routes.py
+++ only_pets/main/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
+from flask_login import login_required
 from only_pets.models import *
 from only_pets.main.forms import *
 from only_pets.extensions import app, db
@@ -14,15 +15,23 @@ main = Blueprint("main", __name__)
 def homepage():
     return render_template('home.html')
 
-# @main.route('/create_user', methods=['GET', 'POST'])
-# @login_required
-# def create_post():
-#     pass
 
-# @main.route('/create_account', methods=['GET', 'POST'])
-#@login_required
-# def create_account():
-#     pass
+@main.route('/create_account', methods=['GET', 'POST'])
+@login_required
+def create_account():
+    form = AccountForm()
+    if form.validate_on_submit():
+        new_account = AccountForm(
+            username=form.username.data,
+            biography=form.biography.data,
+            photo_url=form.photo_url.data,
+        )
+        db.session.add(new_account)
+        db.session.commit()
+        flash('New Account Created!')
+        return redirect(url_for('main.account_detail', account_id=new_account.id))
+    return render_template('create_account.html', form=form)
+    pass
 
 # @main.route('/create_post', methods=['GET', 'POST'])
 #@login_required
