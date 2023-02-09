367e1608a08ef5e8d538e72d2e058f0dc421baec
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-09 12:49:24 -0500

    took out something from user

--- only_pets/models.py
+++ only_pets/models.py
@@ -10,13 +10,27 @@ from datetime import datetime
 #           Models                      #
 ##########################################
 
-class User(UserMixin, db.Model):
-    """User model."""
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    posts = db.relationship('Posts', backref='author', lazy=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     # users_account = db.relationship('Account', secondary='user_account', 
     # back_populates='users')
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 class Account(UserMixin, db.Model):
     """Account model."""
