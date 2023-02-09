cc006111fbfcbed00fb26447a694553b669165cd
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-09 13:14:15 -0500

    finished user form

--- only_pets/main/forms.py
+++ only_pets/main/forms.py
@@ -7,7 +7,28 @@ from only_pets.models import *
 
 class UserForm(FlaskForm):
     """Form for adding/updating a User."""
-    pass
+    name = StringField('Name', 
+    validators=[
+        DataRequired(), 
+        Length(min=3, max=80, message="Your name needs to be betweeen 3 and 80 chars")
+    ])
+    email = StringField('Email', 
+    validators=[
+        DataRequired(), 
+        Length(min=3, max=80, message="Your email needs to be betweeen 3 and 80 chars")
+    ])
+    username = StringField('Username', 
+    validators=[
+        DataRequired(), 
+        Length(min=3, max=80, message="Your username needs to be betweeen 3 and 80 chars")
+    ])
+    password = StringField('Password', 
+    validators=[
+        DataRequired(), 
+        Length(min=3, max=60, message="Your password needs to be betweeen 3 and 80 chars")
+    ])
+    submit = SubmitField('Submit')
+
 
 class AccountForm(FlaskForm):
     """Form for adding/updating an account."""
