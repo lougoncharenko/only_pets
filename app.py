36d76f334477c99907b38d92b67a83c16b1c6644
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-09 13:07:34 -0500

    specified which port to use

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app.register_blueprint(main)
 app.register_blueprint(auth)
 
 with app.app_context():
-    # db.drop_all()
+    #db.drop_all()
     db.create_all()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port = 3002)
     
